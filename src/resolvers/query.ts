2d793c4eafee00e881d22e1624aa96a31d487458
ghostbustermx <keconecte@gmail.com>
2024-04-07 17:58:28 -0700

    resolvers query information from selected course

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -20,6 +20,24 @@ const query : IResolvers = {
         },
         cursos(): any {
             return database.cursos;
+        },
+        curso(__: void, { curso }): any {
+            const resultado = database.cursos.filter(curso_ => curso_.id === curso) [0];
+            if (resultado === undefined){
+                return {
+                    id: '-1',
+                    title: `No se ha encontrado el curso con el ID ${curso}`,
+                    description: '',
+                    clases: -1,
+                    time: 0.0,
+                    logo: '',
+                    level: 'TODOS',
+                    path: '',
+                    teacher: '',
+                    reviews: []
+                }
+            }
+            return resultado;
         }
     }
 }
