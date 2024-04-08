ee0bf385ba99764421f3e348c6dbf492469f1bee
ghostbustermx <keconecte@gmail.com>
2024-04-07 17:44:45 -0700

    resolvers query courses list

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -17,6 +17,9 @@ const query : IResolvers = {
                 }
             }
             return resultado;
+        },
+        cursos(): any {
+            return database.cursos;
         }
     }
 }
