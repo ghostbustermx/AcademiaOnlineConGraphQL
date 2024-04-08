18008da6dd539b563000045d4f567b2bdf998946
ghostbustermx <keconecte@gmail.com>
2024-04-07 19:21:36 -0700

    mutation add a course with duplicate name validation

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,43 @@
 import { IResolvers } from 'graphql-tools';
 import _ from 'lodash';
+import { database } from '../data/data.store';
 
 const mutation : IResolvers = {
-    
+    Mutation: {
+        cursoNuevo(__:void, { curso }): any {
+            const itemCurso = {
+                id: String(database.cursos.length +1),
+                title: curso.title,
+                description: curso.description,
+                clases: curso.clases,
+                time: curso.time,
+                level: curso.level,
+                logo: curso.logo,
+                path: curso.path,
+                teacher: curso.teacher,
+                reviews: []
+            }
+
+            if (database.cursos.filter(itemCurs => itemCurs.title === itemCurso.title).length === 0) {
+                database.cursos.push(itemCurso);
+                return itemCurso;
+            }
+
+            
+            return {
+                id: '-1',
+                title: `El curso ya existe con este titulo`,
+                description: '',
+                clases: -1,
+                time: 0.0,
+                level: 'TODOS',
+                logo: '',
+                path: '',
+                teacher: '',
+                reviews: []
+            };
+        }
+    }
 }
 
 export default mutation;
