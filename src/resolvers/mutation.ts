9f3e115bbeb80a92237c79013d3522225b994221
ghostbustermx <keconecte@gmail.com>
2024-04-07 19:41:23 -0700

    resolvers mutation course modify

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -36,6 +36,31 @@ const mutation : IResolvers = {
                 teacher: '',
                 reviews: []
             };
+        },
+        modificarCurso(__:void, { curso }): any{
+            const elementoExiste = _.findIndex(database.cursos, function(o) {
+                return o.id === curso.id
+            });
+
+           if (elementoExiste > -1) {
+                const valoraciones = database.cursos[elementoExiste].reviews;
+                curso.reviews = valoraciones;
+                database.cursos[elementoExiste] = curso;
+                return curso;
+           }
+
+           return {
+            id: '-1',
+            title: `El curso no existe en la base de datos`,
+            description: '',
+            clases: -1,
+            time: 0.0,
+            level: 'TODOS',
+            logo: '',
+            path: '',
+            teacher: '',
+            reviews: []
+        };
         }
     }
 }
